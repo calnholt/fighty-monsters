725661dceefdd6c45337bc1ed2fb229fa522f2cc
Calvin Holt <calnholt@gmail.com>
2021-04-07 22:21:05 -0400

    new properties and json export object

--- src/app/modules/monster/monster/monster.ts
+++ src/app/modules/monster/monster/monster.ts
@@ -1,14 +1,22 @@
-import { Path } from './../../data/data';
+import { Path, BackgroundImageType } from './../../data/data';
+
 export class Monster {
     name: string;
     speed: number;
     defense: number;
     actions: Array<Action> = new Array();
-    imagePath: Path;
-}
+    cardBackgroundImage: BackgroundImageType;
+    monsterBackgroundImage: BackgroundImageType;
 
+}
 export class Action {
     isDirect: boolean;
     text: string;
     innateText?: string;
+}
+export class MonstersJson {
+    monsters: Array<Monster>;
+    constructor(monsters: Array<Monster>) {
+        this.monsters = monsters;
+    }
 }
